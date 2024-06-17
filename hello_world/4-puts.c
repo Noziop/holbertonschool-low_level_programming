e08681365ababb412bb9d515df78c8f86e236f4b
Noziop <contact@fassih.ch>
2024-06-17 17:13:37 +0200

    first programm in C is not printing 'hello world'

---
+++ hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/* including the std input/ouput lib */
+/* launching the main function*/
+int main (void) {
+    /* Using puts() to display the message */
+    puts("Programming is like building a multilingual puzzle");
+    /*return 0 to exit*/
+    return 0;
+}
