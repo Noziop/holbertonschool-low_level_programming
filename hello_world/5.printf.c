0a8d230fcbac2a7a162335ce9cfa5d36730c8837
Noziop <contact@fassih.ch>
2024-06-18 12:12:18 +0200

    printing the sie in Bytes of $, in C

---
+++ hello_world/5.printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - This is the entry point
+ *
+ * Description: uses printf to display test, followed by a new line
+ * Return: 0 successful
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
---
+++ hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - this is the entry point
+ *
+ * Description: prints the different size of variable
+ * Return: 0 Successfull
+ */
+int main(void)
+{
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
+
+	return (0);
+}
