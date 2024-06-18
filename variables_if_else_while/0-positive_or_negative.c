7edb07771e1ad35c214348b82a872d0885df4fdf
Noziop <contact@fassih.ch>
2024-06-18 16:10:28 +0200

    pushing directory created for project 2

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: using srand() and rand() to print a random number
+ * time: is use to return current time as second for a random number
+ * Return: 0 successful
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf(" %d is positive\n", n);
+	else if (n == 0)
+		printf(" %d is zero\n", n);
+	else
+		printf(" %d is negative\n", n);
+
+	return (0);
+}
