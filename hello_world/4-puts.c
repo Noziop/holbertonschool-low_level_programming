aa66c5ea212847bfc80e1a33f1c2fba8b8b84508
Noziop <contact@fassih.ch>
2024-06-18 10:26:02 +0200

    betty coding style for the 1st time

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,11 @@
-/* including the std input/ouput lib */
 #include <stdio.h>
-/* launching the main function*/
+/**
+ * main - this is the entry entry point
+ * Description - prints "Programming is like building a multilingal puzzle
+ * Return: 0 Successful
+ */
 int main(void)
 {
-/* Using puts() to display the message */
-puts("\"Programming is like building a multilingual puzzle");
-/*return 0 to exit*/
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
