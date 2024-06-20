ec5edb70fb2f757bcd2e4692dfd173829dd7178f
Noziop <contact@fassih.ch>
2024-06-20 15:09:15 +0200

    pushing all files

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,10 +1,17 @@
 #ifndef main_h
 #define main_h
+#include <stdio.h>
+#include <unistd.h>
 /**
 * _putchar - writes the character c to stdout
-*
+ * @c: The character to print
+
+* print_alphabet - write the alphabet using _putchar
 */
 
 int _putchar(char c);
+
+void print_alphabet(void);
+
 
 #endif
