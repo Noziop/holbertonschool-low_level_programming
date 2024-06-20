ccc39fb95f82579085d4b00c7d00be0f7cf9af7f
Noziop <contact@fassih.ch>
2024-06-20 15:52:14 +0200

    exercise 2 printing 10x alphabet

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_alphabetx10 - writes the alphabet 10 times to stdout
+ *
+ *
+ */
+void print_alphabet_x10(void)
+{
+
+	int i;
+
+
+
+	while (i < 10)
+	{
+		char az;
+		az = 'a';
+		while (az < 'z')
+		{
+			_putchar(az);
+			az++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
