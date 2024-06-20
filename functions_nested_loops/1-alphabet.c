5f780890447f5881693e41a68f587b2915f6d070
Noziop <contact@fassih.ch>
2024-06-20 14:38:06 +0200

    printing with alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * 
+ */
+void print_alphabet(void)
+{
+	char az[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i <= 26; i++)
+	{
+		_putchar(az[i]);
+	}
+	_putchar('\n');
+}
