e884e8d895d20d7109bbae3b1c51b1a36a9a1c61
Noziop <contact@fassih.ch>
2024-06-21 14:23:26 +0200

    C program that helps Jack Bauer to remember a day is only 24h long

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * jack_bauer - every mintues in a 24h time
+ *
+ * Description: print every minute from 00:00 to 23:59
+ * Return: every minutes
+ */
+
+void jack_bauer(void)
+{
+	int hour, minutes;
+
+	for (hour = 0; hour <= 23; hour++)
+	{
+		for (minutes = 0; minutes <= 59; minutes++)
+		{
+			_putchar(hour / 10 + '0');
+			_putchar(hour % 10 + '0');
+			_putchar(' ');
+			_putchar(':');
+			_putchar(' ');
+			_putchar(minutes / 10 + '0');
+			_putchar(minutes % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
