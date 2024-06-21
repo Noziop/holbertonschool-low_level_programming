3c7b2b2538f623c1bd8dbeeebdc96c426343379c
Noziop <contact@fassih.ch>
2024-06-21 16:06:11 +0200

    c programm to make an addition from integers

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -3,8 +3,8 @@
 
 /**
  * add - entry point
- * @x: values to add each other.
- * @y: 
+ *@x: values to add to y.
+ *@y: value to add to x
  *
  * Description: does an addition
  * Return: int to main.
