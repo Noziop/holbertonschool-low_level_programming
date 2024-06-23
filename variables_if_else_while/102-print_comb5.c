8ead0cd0117e153fffed4a5019875aa18ae43262
Noziop <contact@fassih.ch>
2024-06-23 22:07:50 +0200

    forgot to remove simple quote on 0 :facepalm:

--- variables_if_else_while/102-print_comb5.c
+++ variables_if_else_while/102-print_comb5.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	int i,j;
 
-	for (i = '0'; i <= 98; i++)
+	for (i = 0; i <= 98; i++)
 	{
 		for (j = i + 1; j <= 99; j++)
 		{
@@ -24,12 +24,12 @@ int main(void)
 				{
 					continue;
 				}
-			
+
 			putchar(',');
 			putchar(' ');
 		}
 	}
-	
+
 	putchar('\n');
 
 	return(0);
