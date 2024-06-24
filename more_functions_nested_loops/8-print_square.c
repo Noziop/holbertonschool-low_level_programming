ee0e4a81d196a81d37e485612154a3fd50dfec75
Noziop <contact@fassih.ch>
2024-06-24 15:53:53 +0200

    correcting both loops, had an extra # printing + unneeded newline

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -15,15 +15,14 @@ void print_square(int size)
 	}
 	else
 	{
-		for (i = 0; i <= size; i++)
+		for (i = 0; i < size; i++)
 		{
-			for (j = 0; j <= size; j++)
+			for (j = 1; j < size; j++)
 			{
 				_putchar('#');
 			}
 			_putchar('#');
 			_putchar('\n');
 		}
-	_putchar('\n');
 	}
 }
