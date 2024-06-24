dc77d6135690ca143016dadd8c8b25affac8c635
Noziop <contact@fassih.ch>
2024-06-24 10:38:38 +0200

    chcking if a char is upper or lower

---
+++ more_functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+#ifndef main_h
+#define main_h
+/**
+* _putchar - writes the character c to stdout
+ * @c: The character to print
+
+* print_alphabet - write the alphabet using _putchar
+*/
+
+int _putchar(char c);
+
+int _isupper(int c);
+
+#endif
