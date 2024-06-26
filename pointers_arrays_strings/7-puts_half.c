9ac234a63120f574f21f19a4acc554986e982002
Noziop <contact@fassih.ch>
2024-06-26 15:08:14 +0200

    printing hal of a string

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -16,3 +16,19 @@ void puts_half(char *str)
 	}
 	_putchar('\n');
 }
+
+/**
+ * _strlen - calculate the length of a string
+ *@s: string to be tested
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	return (i);
+}
