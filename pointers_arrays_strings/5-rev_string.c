ea1d107b01c0f493968ff4c347acad55e448028b
Noziop <contact@fassih.ch>
2024-06-26 09:50:41 +0200

    correction adding print_rev() to reverse the string inside the file, for the checker to find

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -8,3 +8,24 @@ void rev_string(char *s)
 {
 	print_rev(s);
 }
+
+/**
+ * print_rev - print a string in reverse mode
+ *@s: string to be printed
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	i--;
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
