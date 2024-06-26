dc3dc7ed26486b2428a0c3662e8dd314651a67c8
Noziop <contact@fassih.ch>
2024-06-26 13:25:21 +0200

    correcting : simplifying the loop with i < _strlen(str), adding a if statement for empty string

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,16 +1,38 @@
 #include "main.h"
 
 /**
- * 
+ * puts2 - function that prints every other character of a string
+ * 			starting with the first character, followed by a new line.
+ * @str: string to be printed
  */
 
 void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i += 2)
+	if (_strlen(str) == 0)
+	{
+		return;
+	}
+	for (i = 0; i < _strlen(str); i += 2)
 	{
 		_putchar(str[i]);
 	}
 	_putchar('\n');
+}
+
+/**
+ * _strlen - calculate the length of a string
+ *@s: string to be tested
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	return (i);
 }
