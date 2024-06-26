053f05debee6c747410bd8e6dcaa8852782e3b01
Noziop <contact@fassih.ch>
2024-06-26 13:48:16 +0200

    correcting somme betty style

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -2,7 +2,7 @@
 
 /**
  * puts2 - function that prints every other character of a string
- * 			starting with the first character, followed by a new line.
+ *			starting with the first character, followed by a new line.
  * @str: string to be printed
  */
 
