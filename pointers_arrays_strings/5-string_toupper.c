142998e7ff00467cacadaacf87d0ea8699eebe10
Noziop <contact@fassih.ch>
2024-06-28 22:41:06 +0200

    forgot to look after a null byte in while loop, seemed obvious to me, but nope, obv

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -9,13 +9,14 @@
 
 char *string_toupper(char *str)
 {
-	int index;
+	int index = 0;
 
-	while (str[index])
+	while (str[index] != '\0')
 	{
 		if (str[index] >= 'a' && str[index] <= 'z')
+		{
 			str[index] -= 32;
-
+		}
 		index++;
 	}
 
