bebd7850bb4afef518a9ca3861bdef77a0a00537
Noziop <contact@fassih.ch>
2024-06-29 15:54:13 +0200

    function that prints an integer

---
+++ more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_number - Prints an integer using _putchar.
+ * @n: The integer to be printed.
+ */
+void print_number(int n)
+{
+	unsigned int num = n;
+	unsigned int divisor;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -n;
+	}
+
+	divisor = 1;
+
+	while (num / divisor >= 10)
+	{
+		divisor *= 10;
+	}
+
+	while (divisor > 0)
+	{
+		int digit = num / divisor;
+
+		_putchar(digit + '0');
+		num %= divisor;
+		divisor /= 10;
+	}
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -31,6 +31,7 @@ void print_square(int size);
 
 void print_triangle(int size);
 
+void print_number(int n);
 
 
 #endif
