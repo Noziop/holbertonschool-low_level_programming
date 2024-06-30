c58b2d2734bfa7803852180920a71e86901bae9e
Noziop <contact@fassih.ch>
2024-06-30 09:19:07 +0200

    struggling, but i'll pass this again.

--- pointers_arrays_strings/102-magic.c
+++ pointers_arrays_strings/102-magic.c
@@ -16,7 +16,7 @@ int main(void)
    * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
-  putchar('a');putchar('[');putchar('2');putchar(']');putchar(' ');putchar('=');putchar(' ');putchar(57);putchar(56);putchar('\n');
+  *(p + 5) = 98;
   /* ...so that this prints 98\n */
   printf("a[2] = %d\n", a[2]);
   return (0);
