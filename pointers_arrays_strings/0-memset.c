b6d08d7e356c56b6a71e117f65380a9fb7ca0892
Noziop <contact@fassih.ch>
2024-07-01 11:36:02 +0200

    program to fill the memory n time with a char

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _memset - fills the memery with b n time
+ * @s: pointer to the memory area
+ * @b: character to fill in memory
+ * @n: The number of bytes to fill
+ *
+ * Return: pointer to the memory area if success
+ *			return 1 if error
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	if (n == 0 || s == NULL)
+	{
+		return (s);
+	}
+
+	while (i <= n)
+	{
+		s[i] = b;
+		i++;
+	}
+
+	return (s);
+}
