576b69fc765ee23ec0b588cd29abce6324a97b73
Noziop <contact@fassih.ch>
2024-07-02 10:57:23 +0200

    modif : gitignore adding files, main.h adding proto for 7 exercise / adding play chess a chessboard in c, printREALchess : just a side project of 7th exercise, trying to print a real chessboard, 8th exercise : print the sum of diagonal in a matrix.

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * print_diagsums - print the sum of a diagonal in a matrice
+ * @a: pointer to the first element of matrix
+ * @size: size of the matrix
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i;
+	long int sum1 = 0, sum2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sum1 += a[i * size + 1];
+		sum2 += a[i * size + (size - 1 - i)];
+	}
+
+	printf("%ld, %ld\n", sum1, sum2);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -37,5 +37,5 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 /* void print_chessboard(char (*a)[8]); */
 void print_REAL_chessboard(char board[8][8]);
-
+void print_diagsums(int *a, int size);
 #endif
