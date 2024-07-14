4f8b9a815b843fc9b588adc9c631c81ee27eba70
Noziop <contact@fassih.ch>
2024-07-14 19:34:51 +0200

    forgot to push the modified dog.h with renamed _strlen funct()

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -27,7 +27,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 char *_strncpy(char *dest, char *src, int n);
-int __strlen(char *s);
+int _strlen(char *s);
 void free_dog(dog_t *d);
 
 #endif /* DOG_H */
