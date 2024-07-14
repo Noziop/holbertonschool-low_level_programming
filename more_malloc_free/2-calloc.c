5113c4fbf34e8aac4a73e6bb13b0e5ee0bb2347d
Noziop <contact@fassih.ch>
2024-07-14 18:25:00 +0200

    forgot to initialise pointer to 0

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _calloc - Allocates memory for an array, using calloc.
+ * _calloc - Allocates memory for an array, using malloc.
  * @nmemb: The number of elements.
  * @size: The size of each element.
  *
@@ -20,6 +20,36 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (ptr == NULL || nmemb == 0 || size == 0)
 		return (NULL);
 
+	_memset(ptr, 0, total_size);
+
 	return (ptr);
 	free(ptr);
+}
+
+/**
+ * _memset - fills the memery with b n time
+ * @s: pointer to the memory area
+ * @b: character to fill in memory
+ * @n: The number of bytes to fill
+ *
+ * Return: pointer to the memory area if success
+ *			return 1 if error
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	if (n == 0 || s == NULL)
+	{
+		return (s);
+	}
+
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+
+	return (s);
 }
