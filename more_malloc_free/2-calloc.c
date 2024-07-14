a20c8bb98df688ba9caf09640cf724ac61b93a0d
Noziop <contact@fassih.ch>
2024-07-14 18:32:28 +0200

    bad idea : regrouping conditions and return null. had to really mimic calloc

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -14,10 +14,13 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	void *ptr;
 	unsigned int total_size;
 
+	if (nmemb == 0 || size == 0)
+	return (NULL);
+	
 	total_size = nmemb * size;
 
 	ptr = malloc(total_size);
-	if (ptr == NULL || nmemb == 0 || size == 0)
+	if (ptr == NULL)
 		return (NULL);
 
 	_memset(ptr, 0, total_size);
