ca36b3c677cd60d8dce27ea01e768b870a4a526b
Noziop <contact@fassih.ch>
2024-07-14 19:40:13 +0200

    wrong output with long string in checker

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,49 +1,6 @@
 #include "dog.h"
 
 /**
- * new_dog - Creates a new dog.
- * @name: The name of the dog.
- * @age: The age of the dog.
- * @owner: The owner of the dog.
- *
- * Return: A pointer to the newly created dog_t structure.
- *         If the function fails, returns NULL.
- */
-dog_t *new_dog(char *name, float age, char *owner)
-{
-	dog_t *new_dog;
-	char *new_name;
-	char *new_owner;
-
-	new_dog = malloc(sizeof(dog_t));
-	if (new_dog == NULL)
-		return (NULL);
-
-	new_name = malloc(_strlen(name) + 1);
-	if (new_name == NULL)
-	{
-		free(new_dog);
-		return (NULL);
-	}
-	_strncpy(new_name, name, _strlen(name) + 1);
-
-	new_owner = malloc(_strlen(owner) + 1);
-	if (new_owner == NULL)
-	{
-		free(new_name);
-		free(new_dog);
-		return (NULL);
-	}
-	_strncpy(new_name, name, _strlen(name) + 1);
-
-	new_dog->name = new_name;
-	new_dog->age = age;
-	new_dog->owner = new_owner;
-
-	return (new_dog);
-}
-
-/**
  * _strncpy - Copies a string.
  * @dest: The destination buffer.
  * @src: The source string.
@@ -75,10 +32,53 @@ char *_strncpy(char *dest, char *src, int n)
  */
 int _strlen(char *s)
 {
-    int i;
+	int i;
 
-    for (i = 0; s[i] != '\0'; i++)
-    {
-    }
-    return (i);
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	return (i);
+}
+
+/**
+ * new_dog - Creates a new dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ *
+ * Return: A pointer to the newly created dog_t structure.
+ *			If the function fails, returns NULL.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog;
+	char *new_name;
+	char *new_owner;
+
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+		return (NULL);
+
+	new_name = malloc(_strlen(name) + 1);
+	if (new_name == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+	_strncpy(new_name, name, _strlen(name) + 1);
+
+	new_owner = malloc(_strlen(owner) + 1);
+	if (new_owner == NULL)
+	{
+		free(new_name);
+		free(new_dog);
+		return (NULL);
+	}
+	_strncpy(new_owner, owner, _strlen(owner) + 1);
+
+	new_dog->name = new_name;
+	new_dog->age = age;
+	new_dog->owner = new_owner;
+
+	return (new_dog);
 }
