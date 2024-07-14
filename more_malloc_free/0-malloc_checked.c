a8b5d8614d2eb6d56dd6bedc44b706a0bbb61380
Noziop <contact@fassih.ch>
2024-07-14 17:26:31 +0200

    creating readme.md + main.h + programm that allocates memeroy using malloc

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * malloc_checked - Allocates memory using malloc.
+ * @b: The number of bytes to allocate.
+ *
+ * Return: A pointer to the allocated memory.
+ *			If malloc fails,
+ *			the function causes normal process termination
+ *			with a status value of 98.
+ */
+void *malloc_checked(unsigned int b)
+{
+	unsigned int *ptr;
+
+	ptr = malloc(b * sizeof(unsigned int));
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return (0);
+}
