05c5b0a4d101fdfc6a9460151cdbdec15d051256
Noziop <contact@fassih.ch>
2024-07-17 18:12:52 +0200

    correction : comment in French + had a func proto twice declared

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -16,13 +16,12 @@ typedef struct op
 	int (*f)(int a, int b);
 } op_t;
 
-/* Déclarations de fonctions */
+/* functions prototypes */
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
-int (*get_op_func(char *s))(int, int);
 int (*get_op_func(char *s))(int, int);
 
 #endif /* CALC_H */
